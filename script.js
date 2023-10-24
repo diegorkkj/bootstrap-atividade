f12a0e80f0cc9ddd91c4db57c3c07ba5ab910bba
diegorkkj <diegosenaisp@gmail.com>
2023-10-24 08:37:44 -0300

    up

---
+++ script.js
@@ -0,0 +1,29 @@
+const tabela = document.querySelector('.tabela-js')
+axios.get('atividades-pwfe.diegosenaisp.repl.co/funcionarios').then(function (resposta) {
+    getData(resposta.data)
+}).catch(function (error) {
+    console.log(error)
+})
+
+function getData(dados) {
+    dados.map((item) => {
+        tabela.innerHTML += `
+            <tr>
+                <th scope="row">${item.id}</th>
+                <td>${item.nome}</td>
+                <td>${item.cargo}</td>
+                <td>${item.salario}</td>
+                <td> 
+                <span class="material-symbols-outlined custom-icon">
+                    delete
+                </span>
+
+
+                <span class="material-symbols-outlined custom-icon-2">
+                edit
+                </span> </td>
+             </tr>
+               
+                `
+    })
+}
